2b638ec9558edaddd5c2081a458a1fe9299e29c7
Maciek99 <64065294+Maciek99@users.noreply.github.com>
2024-07-24 16:33:21 +0200

    feat: Update clubChart and clubList components
    - Update clubChart.tsx to use updated colors for political clubs
    - Sort clubs in clubList.tsx by membersCount in descending order

--- frontend/components/clubChart.tsx
+++ frontend/components/clubChart.tsx
@@ -21,8 +21,8 @@ const clubColors: { [key: string]: string } = {
   Konfederacja: "#1A1A1A",
   "Polska 2050": "#FCD300",
   "Kukiz'15": "#000000",
-  "PSL-TD": "#009FE3",
-  "Polska2050-TD": "#F091E3",
+  "PSL-TD": "#17A42B",
+  "Polska2050-TD": "#FCD300",
   // Add more clubs and their colors as needed
 };
 
@@ -58,7 +58,7 @@ const ClubsChart: React.FC<ClubsChartProps> = ({ clubs }) => {
             type: "item",
             name: "Mandaty",
             keys: ["name", "y", "color", "label"],
-            data: clubs.map((club) => [
+            data: clubs.sort((a,b) => b.membersCount - a.membersCount).map((club) => [
               club.id,
               club.membersCount,
               clubColors[club.id] || "#CCCCCC", // Use predefined color or default to gray
