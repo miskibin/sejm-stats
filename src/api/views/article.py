8b6ceed9cb3d1339f8c1033344a434f07b929799
Michal Skibinski <michal.skibinski@nokia.com>
2024-11-21 11:07:22 +0100

    fix article creation

--- src/api/views/article.py
+++ src/api/views/article.py
@@ -1,25 +1,39 @@
-from rest_framework import generics, status
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from api.pagination import ApiViewPagination
 from api.serializers.articleSerializer import ArticleListSerializer
 from community_app.models import Article
 from community_app.serializers import ArticleSerializer
 from loguru import logger
-from rest_framework.request import Request
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from rest_framework import viewsets
-
+from rest_framework.exceptions import PermissionDenied
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     pagination_class = ApiViewPagination
     lookup_field = "id"
+    ALLOWED_ORIGIN = 'sejm-stats.pl'
 
     def get_serializer_class(self):
         if self.action == "list":
             return ArticleListSerializer
         return ArticleSerializer
+
+    def check_origin(self, request):
+        """Check if the request origin is allowed"""
+        origin = request.headers.get('Origin', '')
+        logger.debug(f"Request origin: {origin}")
+        
+        if self.action in ['create', 'destroy']:
+            if not origin.endswith(self.ALLOWED_ORIGIN):
+                logger.warning(f"Unauthorized access attempt from origin: {origin}")
+                raise PermissionDenied(f"Operations only allowed from {self.ALLOWED_ORIGIN}")
+
+    def initial(self, request, *args, **kwargs):
+        """Run origin check before any action is processed"""
+        super().initial(request, *args, **kwargs)
+        self.check_origin(request)
 
     @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
     def list(self, request, *args, **kwargs):
@@ -37,9 +51,16 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
+        logger.info(f"Article created successfully from origin: {request.headers.get('Origin')}")
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        logger.info(f"Deleting article {instance.id} from origin: {request.headers.get('Origin')}")
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def perform_create(self, serializer):
         serializer.save()
--- src/community_app/admin.py
+++ src/community_app/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from community_app import models
+
+
+@admin.register(models.Article)
+class Article(admin.ModelAdmin):
+    list_display = ("title",)
